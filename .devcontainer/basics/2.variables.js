ca9b729d918e336c3b5210c0ac5218999fe4dda4
DIVYANSH SHARMA <113305434+Divyansh9783@users.noreply.github.com>
2024-09-29 08:54:24 +0000

    variables

---
+++ .devcontainer/basics/2.variables.js
@@ -0,0 +1,20 @@
+const accountId= 144553
+let accountEmail="divyansh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;// Here we have not given any value to the accountState so its value will be undefined 
+//accountId=2
+//the value of the const keyword cannot be changed //Not allowed
+/*
+Here we use let over var as the var is having a scope problem in block scope and functional scope
+
+
+*/
+accountEmail="ds@ds.com"
+accountPassword="21142243"
+accountCity="Bangaluru"
+
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
