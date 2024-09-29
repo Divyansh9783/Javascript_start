11aa576b94593e20984839110fd7de06b61de8a7
DIVYANSH SHARMA <113305434+Divyansh9783@users.noreply.github.com>
2024-09-29 08:54:50 +0000

    Data types

---
+++ .devcontainer/basics/3.DataTypes.js
@@ -0,0 +1,34 @@
+"use strict" //Treat all JS code as newer version of the JS
+//alert(3+3)  //We are using node js not browser
+
+console.log(3+
+    3)
+    //Code readiblity should be high and the code which is written above has the less code readiblity
+
+console.log("Divyansh")
+
+//Datatypes
+//Primtive
+let name="Divyansh"//String
+let age=19 //number=>2 to the power 53
+let isloggedin=false//Boolean
+console.log(isloggedin)
+
+//bigint=>sometimes we use the bigint when we want to store the big data.
+//null=> the null is a standalone value it means that in which hum ne kuch assign kia hai which is null
+//null is undefined nhi hai khali hai 
+//undefined=> this is a value when we do not assign a value to the variable
+//symbol=>for uniqueness and used in react and figma tools
+
+
+//OBJECT
+console.log(typeof "hai")
+console.log(typeof null)
+console.log(typeof undefined);
+
+
+//The null is a object 
+//The undefined is a datatype
+
+
+
