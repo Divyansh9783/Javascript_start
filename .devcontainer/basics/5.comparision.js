b7a75c8531c460f63b98125ef760b838984dabbf
DIVYANSH SHARMA <113305434+Divyansh9783@users.noreply.github.com>
2024-09-30 03:27:45 +0000

    Comparasion

---
+++ .devcontainer/basics/5.comparision.js
@@ -0,0 +1,25 @@
+//console.log(2>1);
+
+console.log("2">1);
+console.log("02">1);
+//First JS converts the string to the integer and then compares it
+//When we compare two variables the data type should be same 
+
+console.log(null>0);//False
+console.log(null==0);//False
+console.log(null>=0);//True
+
+console.log(undefined>0);//False
+console.log(undefined==0);//False
+console.log(undefined<0);//False
+
+// ===
+console.log("2"===2);
+console.log("2"==2);
+//The === checks the data type also 
+
+
+
+
+
+
