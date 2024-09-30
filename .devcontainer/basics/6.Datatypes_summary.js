cd9079f26232e0bc667bf5b1972a62ad76c49a66
DIVYANSH SHARMA <113305434+Divyansh9783@users.noreply.github.com>
2024-09-30 03:28:20 +0000

    Datatypes_Summary

---
+++ .devcontainer/basics/6.Datatypes_summary.js
@@ -0,0 +1,39 @@
+//  Primitive 
+//7 Types : string,number,boolean,null,undefined,symbol,BigInt
+
+
+const score=100
+const scorevalue=100.3
+const isloggedin=false
+const outsideTemp=null
+let userEmail;//Undefined
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id);
+console.log(anotherid);
+console.log(id===anotherid);
+
+const bignumber=23243434343444444445n
+
+
+
+
+
+//JS is Dynamically Typed language
+//Reference (Non primitive)
+//Array,Objects,Functions
+
+
+const heros=["IronMan","NaagRaj","SuperMan"];
+let myObj={
+    name:"Div",
+    age:22,
+
+}
+const myFunstion=function(){
+    console.log("hello World");
+    
+}
+
+console.log(typeof outsideTemp);
+
